5a85896c6987f48420a99148ccba4b7f2b2cf000
jaehyunpark <wogus3602@naver.com>
2019-09-13 13:54:48 +0900

    강화학습

--- RL practice/test.py
+++ RL practice/test.py
@@ -1,9 +1,1 @@
-import gym
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
-
-# env에 기초하여 Input, output 사이즈 정하기
-np.identity(16)[5:5 + 1]
-print(np.identity(16)[3:3 + 1])
+import tensorflow
